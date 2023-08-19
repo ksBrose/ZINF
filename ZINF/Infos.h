fdc6a619ff6e9d7348657bf4d902ee2c9c94c2d4
dudacasali <124858995+dudacasali@users.noreply.github.com>
2023-08-18 22:51:07 -0300

    Add files via upload

--- ZINF/Infos.h
+++ ZINF/Infos.h
@@ -13,10 +13,8 @@ typedef struct informacoes
 {
     GameScreen currentState;
     Rectangle gameArea;
-    bool won;
     bool closeGame;
     int currentLevel;
-    int optionSelected;
     int score;
     int num_inimigos;
     int num_inimigos_mortos;
